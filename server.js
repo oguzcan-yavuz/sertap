fa8a69dd936e2c7dda5729f4126cfad033f86952
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-06-06 00:34:03 +0300

    favicon, static and views configs completed

--- server.js
+++ server.js
@@ -3,13 +3,19 @@
 require('dotenv').load();
 const express = require('express');
 const logger = require('morgan');
+const favicon = require('serve-favicon');
+const path = require('path');
 const router = require('./app/routes/index.js');
 const PORT = process.env.PORT || 3000;
 
 let app = express();
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.urlencoded({ extended:true }));
 app.use(router);
 app.listen(PORT, () => {
-  console.log("Server is listening on " + PORT);
+  console.log("Server is listening on port " + PORT);
 });
