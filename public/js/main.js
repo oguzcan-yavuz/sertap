f1b6a84b4e6d5e6509f9db3cb943cc10ba03adcb
Yiğit Altıntaş <yigitaltitnas@outlook.com.tr>
2018-06-06 02:08:11 +0300

    fetch ile dosya gonderme fonksiyonu eklendi.

--- public/js/main.js
+++ public/js/main.js
@@ -48,7 +48,26 @@ function createDownloadLink() {
     li.appendChild(au);
     li.appendChild(hf);
     recordingslist.appendChild(li);
+    sendFile('/',{soundFile: url});
   });
+}
+
+/**
+* Ses dosyasını back end e ıletme
+*
+* @return JSON
+*/
+
+function sendFile(url, data) {
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(response => console.log('Success:', response));
 }
 
 window.onload = function init() {
